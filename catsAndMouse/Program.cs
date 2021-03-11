b7229c4a63657de4511c104d3614fd64da7b566e
rzvdev <razviiandrei@gmail.com>
2021-03-11 12:26:57 +0200

    hackerank exercises 2

---
+++ catsAndMouse/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace catsAndMouse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(catsAndMouse(1,3,2));
+        }
+
+        static string catsAndMouse(int x, int y, int z){
+            int catA = Math.Abs(z-x);
+            int catB = Math.Abs(z-y);
+
+            if(catA == catB){
+                return "Mouse C";
+            } else if(catA > catB){
+                return "Cat B";
+            } else {
+                return "Cat A";
+            }
+
+                         
+        }
+    }
+}
