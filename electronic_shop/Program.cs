2f4e19df2f1838350f224d3d62f867d1978d7ff4
rzvdev <razviiandrei@gmail.com>
2021-03-11 12:04:40 +0200

    hackrank exercises 1

---
+++ electronic_shop/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace electroni_shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetMoneySpent(new int[]{3},new int[]{8}, 10));
+            Console.ReadLine(); 
+        }
+
+
+        public static int GetMoneySpent(int[] keyboards, int[] drivers, int b){
+            int maxPrice = -1;
+
+            for(int i = 0; i < keyboards.Length; i++){
+                for(int j = 0; j < drivers.Length; j++){
+                    if((keyboards[i] + drivers[j]) > maxPrice && keyboards[i] + drivers[j] <= b){
+                        maxPrice = keyboards[i] + drivers[j];
+                    } 
+                }
+            }
+            return maxPrice;
+        }
+    }
+}
