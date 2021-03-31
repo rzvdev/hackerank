7984026e24de06ccc1e104adbb01c39b8c24b2c4
Razvi Andre <razviiandre@yahoo.ro>
2021-03-31 13:53:28 +0300

    Add project files.

---
+++ hurdleRace/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace hackerank.hurdleRace
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write(hurdleRace(4,new int[] { 1,6,3,5,2}));
+        }
+        
+        static int hurdleRace(int k, int[] height)
+        {
+            int doses = 0;
+            
+            for (int i = 0; i < height.Length; i++)
+            {
+                int difference = height[i] - k;
+
+                    if(difference > 0)
+                {
+                    doses += difference;
+                    k += difference;
+                }
+            }
+
+            return doses;
+        }
+    }
+}
---
+++ pickingNumbers/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace hackerank.pickingNumbers
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(pickingNumbers(new List<int>(){1,1,2,2,4,4,5,5,5}));            
+        }
+        
+        
+        public static int pickingNumbers(List<int> a)
+        {
+            int[] listArray = a.ToArray();
+            int maxCounted = 0;
+            int counter = 0;
+
+            for (int i = 0; i < listArray.Length - 1; i++)
+            {
+                int iElement = listArray[i];
+                for (int j = 0; j < listArray.Length; j++)
+                {
+                    int jElement = listArray[j];
+                    int difference = iElement - jElement;
+
+                    if (difference == 0 || difference == -1)
+                    {
+                        counter++;
+                    }
+                }
+                if (counter > maxCounted)
+                {
+                    maxCounted = counter;
+                }
+
+                counter = 0;
+            }
+            return maxCounted;
+        }
+    }
+}
